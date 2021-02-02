c03ad6534f5f159505d7cd284ca98c3c685323a2
longyi5243 <525545144@qq.com>
2021-02-02 11:43:26 +0800

    no message

--- vue-front/pages/register.vue
+++ vue-front/pages/register.vue
@@ -9,31 +9,36 @@
     <div class="sign-up-container">
       <el-form ref="userForm" :model="params">
 
-        <el-form-item class="input-prepend restyle" prop="nickname" :rules="[{ required: true, message: '请输入你的昵称', trigger: 'blur' }]">
+        <el-form-item class="input-prepend restyle" prop="nickname"
+                      :rules="[{ required: true, message: '请输入你的昵称', trigger: 'blur' }]">
           <div>
             <el-input type="text" placeholder="你的昵称" v-model="params.nickname"/>
             <i class="iconfont icon-user"/>
           </div>
         </el-form-item>
 
-        <el-form-item class="input-prepend restyle no-radius" prop="mobile" :rules="[{ required: true, message: '请输入手机号码', trigger: 'blur' },{validator: checkPhone, trigger: 'blur'}]">
+        <el-form-item class="input-prepend restyle no-radius" prop="mobile"
+                      :rules="[{ required: true, message: '请输入手机号码', trigger: 'blur' },{validator: checkPhone, trigger: 'blur'}]">
           <div>
             <el-input type="text" placeholder="手机号" v-model="params.mobile"/>
             <i class="iconfont icon-phone"/>
           </div>
         </el-form-item>
 
-        <el-form-item class="input-prepend restyle no-radius" prop="code" :rules="[{ required: true, message: '请输入验证码', trigger: 'blur' }]">
+        <el-form-item class="input-prepend restyle no-radius" prop="code"
+                      :rules="[{ required: true, message: '请输入验证码', trigger: 'blur' }]">
           <div style="width: 100%;display: block;float: left;position: relative">
             <el-input type="text" placeholder="验证码" v-model="params.code"/>
             <i class="iconfont icon-phone"/>
           </div>
           <div class="btn" style="position:absolute;right: 0;top: 6px;width: 40%;">
-            <a href="javascript:" type="button" @click="getCodeFun()" :value="codeTest" style="border: none;background-color: none">{{codeTest}}</a>
+            <a href="javascript:" type="button" @click="getCodeFun()" :value="codeTest"
+               style="border: none;background-color: none">{{codeTest}}</a>
           </div>
         </el-form-item>
 
-        <el-form-item class="input-prepend" prop="password" :rules="[{ required: true, message: '请输入密码', trigger: 'blur' }]">
+        <el-form-item class="input-prepend" prop="password"
+                      :rules="[{ required: true, message: '请输入密码', trigger: 'blur' }]">
           <div>
             <el-input type="password" placeholder="设置密码" v-model="params.password"/>
             <i class="iconfont icon-password"/>
@@ -65,34 +70,84 @@
 </template>
 
 <script>
-import '~/assets/css/sign.css'
-import '~/assets/css/iconfont.css'
+  import '~/assets/css/sign.css'
+  import '~/assets/css/iconfont.css'
 
-export default {
-  layout: 'sign',
-  data() {
-    return {
-      params: {
-        mobile: '',
-        code: '',
-        nickname: '',
-        password: ''
-      },
-      sending: true,      //是否发送验证码
-      second: 60,        //倒计时间
-      codeTest: '获取验证码'
-    }
-  },
-  methods: {
+  import registerApi from '@/api/register.js'
 
-
-    checkPhone (rule, value, callback) {
-      //debugger
-      if (!(/^1[34578]\d{9}$/.test(value))) {
-        return callback(new Error('手机号码格式不正确'))
+  export default {
+    layout: 'sign',
+    data() {
+      return {
+        params: {
+          mobile: '',
+          code: '',
+          nickname: '',
+          password: ''
+        },
+        sending: true,      //是否发送验证码
+        second: 60,        //倒计时间
+        codeTest: '获取验证码'
       }
-      return callback()
+    },
+    methods: {
+
+      getCodeFun() {
+        //sending = false
+        //his.sending原为true,请求成功，!this.sending == true，主要是防止有人把disabled属性去掉，多次点击；
+        if (!this.sending)
+          return;
+
+        //debugger
+        // prop 换成你想监听的prop字段
+        this.$refs.userForm.validateField('mobile', (errMsg) => {
+          if (errMsg == '') {
+            registerApi.sendCode(this.params.mobile).then(res => {
+              this.sending = false;
+              this.timeDown();
+            });
+          }
+        })
+      },
+
+      //会员注册
+      submitRegister() {
+        this.$refs['userForm'].validate((valid) => {
+          if (valid) {
+            registerApi.registerMember(this.params).then(response => {
+              //提示注册成功
+              this.$message({
+                type: 'success',
+                message: "注册成功"
+              })
+              this.$router.push({path: '/login'})
+            })
+          }
+        })
+      },
+
+      timeDown() {
+        let result = setInterval(() => {
+          --this.second;
+          this.codeTest = this.second
+          if (this.second < 1) {
+            clearInterval(result);
+            this.sending = true;
+            //this.disabled = false;
+            this.second = 60;
+            this.codeTest = "获取验证码"
+          }
+        }, 1000);
+      },
+
+
+      checkPhone(rule, value, callback) {
+        //debugger
+        if (!(/^1[34578]\d{9}$/.test(value))) {
+          return callback(new Error('手机号码格式不正确'))
+        }
+        return callback()
+      }
     }
   }
-}
 </script>
