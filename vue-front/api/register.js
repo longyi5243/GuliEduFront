7512364d70aadd7f5a0a28123d2c69cdcc8647a9
longyi5243 <525545144@qq.com>
2021-02-02 10:16:47 +0800

    no message

---
+++ vue-front/api/register.js
@@ -0,0 +1,23 @@
+import request from '@/utils/request'
+
+export default {
+
+  //发送验证码
+  registerMember(formItem) {
+    return request({
+      url: '/educenter/member/register',
+      method: 'post',
+      data: formItem
+    })
+  },
+
+  //发送验证码
+  sendCode(phone) {
+    return request({
+      url: `/edumsm/msm/send/${phone}`,
+      method: 'get'
+    })
+  }
+
+
+}
