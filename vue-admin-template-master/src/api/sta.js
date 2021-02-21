34a35f06881650af908b363866a32b2dc933f45d
longyi5243 <525545144@qq.com>
2021-02-22 00:52:14 +0800

    no message

--- vue-admin-template-master/src/api/sta.js
+++ vue-admin-template-master/src/api/sta.js
@@ -7,5 +7,12 @@ export default {
       url: `/staservice/sta/registerCount/${day}`,
       method: 'post'
     })
+  },
+  //图表显示  返回两部分数组
+  getDataSta(searchObj) {
+    return request({
+      url: `/staservice/sta/showData/${searchObj.type}/${searchObj.begin}/${searchObj.end}`,
+      method: 'get'
+    })
   }
 }
