6ace74057d48ef9dfbb3bad6d0a4058809a47c25
longyi5243 <525545144@qq.com>
2021-02-23 00:48:06 +0800

    no message

--- vue-admin-template-master/config/dev.env.js
+++ vue-admin-template-master/config/dev.env.js
@@ -5,6 +5,6 @@ const prodEnv = require('./prod.env')
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
   // BASE_API: '"https://easy-mock.com/mock/5950a2419adc231f356a6636/vue-admin"',
-  BASE_API: '"http://localhost:9001"', //所有请求转发到nginx
+  BASE_API: '"http://localhost:8222"', //所有请求转发到nginx
   OSS_PATH: '"https://edu-client.oss-cn-beijing.aliyuncs.com"'
 })
